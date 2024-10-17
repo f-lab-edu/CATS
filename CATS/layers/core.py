68fe3a74d9e25cff0aa7d8ca6b665e2616623963
22ema <yunsung9503@gmail.com>
2024-10-17 17:58:36 +0900

    [feat] PredictionLayer 클래스 구현

--- CATS/layers/core.py
+++ CATS/layers/core.py
@@ -40,3 +40,29 @@ class DNN(nn.Module):
             fc = self.dropout(fc)
             x = fc
         return x
+
+
+class PredictionLayer(nn.Module):
+    """
+      Arguments
+         - **task**: str, ``"binary"`` for  binary logloss or  ``"regression"`` for regression loss
+         - **use_bias**: bool.Whether add bias term or not.
+    """
+
+    def __init__(self, task='binary', use_bias=True, **kwargs):
+        if task not in ["binary", "multiclass", "regression"]:
+            raise ValueError("task must be binary,multiclass or regression")
+
+        super(PredictionLayer, self).__init__()
+        self.use_bias = use_bias
+        self.task = task
+        if self.use_bias:
+            self.bias = nn.Parameter(torch.zeros((1,)))
+
+    def forward(self, X):
+        output = X
+        if self.use_bias:
+            output += self.bias
+        if self.task == "binary":
+            output = torch.sigmoid(output)
+        return output
