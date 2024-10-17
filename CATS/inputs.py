140c50135c8ee7bf3d5acfea46cfdff5304d5142
22ema <yunsung9503@gmail.com>
2024-10-17 14:42:23 +0900

    [feat] DenseFeat 클래스 구현 및 build_input_featues(), get_feature_names()함수 구현

--- CATS/inputs.py
+++ CATS/inputs.py
@@ -1,6 +1,7 @@
-from collections import namedtuple
+from collections import namedtuple, OrderedDict
 
 DEFAULT_GROUP_NAME = "default_group"
+
 
 class SparseFeat(namedtuple('SparseFeat',
                             ['name', 'vocabulary_size', 'embedding_dim', 'use_hash', 'dtype','embedding_name',
@@ -22,4 +23,39 @@ class SparseFeat(namedtuple('SparseFeat',
     def __hash__(self):
         return self.name.__hash__()
 
+
+class DenseFeat(namedtuple('DenseFeat', ['name', 'dimension', 'dtype'])):
+    __slots__ = ()
+
+    def __new__(cls, name, dimension=1, dtype="float32"):
+        return super(DenseFeat, cls).__new__(cls, name, dimension, dtype)
+
+    def __hash__(self):
+        return self.name.__hash__()
+
+
+def build_input_features(feature_columns):
+    # Return OrderedDict: {feature_name:(start, start+dimension)}
+
+    features = OrderedDict()
+
+    start = 0
+    for feat in feature_columns:
+        feat_name = feat.name
+        if feat_name in features:
+            continue
+        if isinstance(feat, SparseFeat):
+            features[feat_name] = (start, start + 1)
+            start += 1
+        elif isinstance(feat, DenseFeat):
+            features[feat_name] = (start, start + feat.dimension)
+            start += feat.dimension
+        else:
+            raise TypeError("Invalid feature column type,got", type(feat))
+    return features
+
+
+def get_feature_names(feature_columns):
+    features = build_input_features(feature_columns)
+    return list(features.keys())
         
