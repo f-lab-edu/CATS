e51dfb449bfcd29d9b1681c456c75260a5ed799e
22ema <yunsung9503@gmail.com>
2024-10-17 16:32:47 +0900

    [feat] create_embedding_matrix 함수 구현

--- CATS/inputs.py
+++ CATS/inputs.py
@@ -1,4 +1,5 @@
 from collections import namedtuple, OrderedDict
+import torch.nn as nn
 
 DEFAULT_GROUP_NAME = "default_group"
 
@@ -97,4 +98,28 @@ def build_input_features(feature_columns):
 def get_feature_names(feature_columns):
     features = build_input_features(feature_columns)
     return list(features.keys())
-        
+
+
+def create_embedding_matrix(feature_columns, init_std=0.0001, linear=False, sparse=False, device='cpu'):
+    # Return nn.ModuleDict: for sparse features, {embedding_name: nn.Embedding}
+    # for varlen sparse features, {embedding_name: nn.EmbeddingBag}
+    sparse_feature_columns = list(
+        filter(lambda x: isinstance(x, SparseFeat), feature_columns)) if len(feature_columns) else []
+
+    varlen_sparse_feature_columns = list(
+        filter(lambda x: isinstance(x, VarLenSparseFeat), feature_columns)) if len(feature_columns) else []
+
+    embedding_dict = nn.ModuleDict(
+        {feat.embedding_name: nn.Embedding(feat.vocabulary_size, feat.embedding_dim if not linear else 1, sparse=sparse)
+         for feat in
+         sparse_feature_columns + varlen_sparse_feature_columns}
+    )
+
+    # for feat in varlen_sparse_feature_columns:
+    #     embedding_dict[feat.embedding_name] = nn.EmbeddingBag(
+    #         feat.dimension, embedding_size, sparse=sparse, mode=feat.combiner)
+
+    for tensor in embedding_dict.values():
+        nn.init.normal_(tensor.weight, mean=0, std=init_std)
+
+    return embedding_dict.to(device)
