f5ece3b0ea6b773d9a4bf80bfbcc2b58818b0679
22ema <yunsung9503@gmail.com>
2024-10-17 18:01:29 +0900

    [feat] 간단한 학습 코드 작성

--- run_simple_train.py
+++ run_simple_train.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import torch.cuda
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.model_selection import train_test_split
 
-from CATS.inputs import SparseFeat
+from CATS.inputs import SparseFeat, DenseFeat, get_feature_names
 
 if __name__ == "__main__":
     # chunks = pd.read_csv('../ml_Datasets/criteo_kaggle/train.txt',
@@ -35,8 +37,29 @@ if __name__ == "__main__":
 
     # 2. count #unique features for each sparse field, and record dense feature field name
 
-    fixlen_feature_columns = [SparseFeat(feat, vocabulary_size=data[feat].max()+1, embedding_dim=4)]
+    fixlen_feature_columns = [SparseFeat(feat, vocabulary_size=data[feat].max()+1, embedding_dim=4)
+                              for feat in sparse_features] + [DenseFeat(feat, 1,)
+                                                              for feat in dense_features]
 
+    dnn_feature_columns = fixlen_feature_columns
+    linear_feature_columns = fixlen_feature_columns
+
+    feature_names = get_feature_names(
+        linear_feature_columns + dnn_feature_columns)
+
+    # 3.generate input data for model
+
+    train, test = train_test_split(data, test_size=0.2, random_state=2024)
+    train_model_input = {name: train[name] for name in feature_names}
+    test_model_input = {name: test[name] for name in feature_names}
+
+    # 4.Define Model, train, predict and eval
+
+    device = 'cpu'
+    use_cuda = True
+    if use_cuda and torch.cuda.is_available():
+        print('using CUDA [cuda:0]')
+        device = 'cuda:0'
 
 
 
