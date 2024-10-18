77812e13e546d20324aa4535c871373c7653a6ab
22ema <yunsung9503@gmail.com>
2024-10-18 17:50:51 +0900

    [feat] SparseFeat 클래스 구현

---
+++ CATS/inputs.py
@@ -0,0 +1,24 @@
+from collections import namedtuple
+
+DEFAULT_GROUP_NAME = "default_group"
+
+
+class SparseFeat(namedtuple('SparseFeat',
+                            ['name', 'vocabulary_size', 'embedding_dim', 'use_hash', 'dtype', 'embedding_name',
+                             'group_name'])):
+    __slots__ = ()
+
+    def __new__(cls, name, vocabulary_size, embedding_dim=4, use_hash=False, dtype="int32", embedding_name=None,
+                group_name=DEFAULT_GROUP_NAME):
+        if embedding_name is None:
+            embedding_name = name
+        elif embedding_name == 'auto':
+            embedding_dim = 6 * int(pow(vocabulary_size, 0.25))
+        if use_hash:
+            raise NotImplementedError("Feature hashing is not supported in PyTorch version. "
+                                      "Please use TensorFlow or disable hashing.")
+        return super(SparseFeat, cls).__new__(cls, name, vocabulary_size, embedding_dim, use_hash, dtype,
+                                              embedding_name, group_name)
+
+    def __hash__(self):
+        return self.name.__hash__()
