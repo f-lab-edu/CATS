69b521b3e2ab52767d13c6bec184848d33548df7
22ema <yunsung9503@gmail.com>
2024-12-18 17:00:43 +0900

    [feat] SequencePoolingLayer._sequence_mask() 메소드 구현

--- CATS/layers/sequence.py
+++ CATS/layers/sequence.py
@@ -28,3 +28,21 @@ class SequencePoolingLayer(nn.Module):
         self.device = device
         self.eps = torch.FloatTensor([1e-8]).to(device)
         self.to(device)
+
+    def _sequence_mask(
+        self, lengths: torch.Tensor, max_len: int = None, dtype: type = torch.bool
+    ):
+        """
+        Generate sequence mask. sequence mask's length is max_len.
+        :param lengths: Tensor containing lengths of features
+        :param max_len: maximum length of the sequence. if max_len is None, maximum in lengths
+        :param dtype: mask's data type
+        :return: mask: sequence mask.
+        """
+        if max_len is None:
+            max_len = lengths.max()
+        row_vector = torch.arange(0, max_len, 1).to(lengths.device)
+        matrix = torch.unsqueeze(lengths, dim=-1)
+        mask = row_vector < matrix
+        mask.type(dtype)
+        return mask
