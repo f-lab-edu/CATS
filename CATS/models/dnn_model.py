3ec0e4c07fbb2a324e3e1ac6c968794413022c9e
22ema <yunsung9503@gmail.com>
2025-01-07 16:54:13 +0900

    [feat] l2 정규화 코드 추가

--- CATS/models/dnn_model.py
+++ CATS/models/dnn_model.py
@@ -64,6 +64,14 @@ class DNNModel(BaseModel):
                        device=device)
         dnn_linear_in_feature = dnn_hidden_units[-1]
         self.dnn_linear = nn.Linear(dnn_linear_in_feature, 1, bias=False).to(device)
+        self.add_regularization_weight(
+            filter(
+                lambda x: "weight" in x[0] and "bn" not in x[0],
+                self.dnn.named_parameters(),
+            ),
+            l2=l2_reg_dnn,
+        )
+        self.add_regularization_weight(self.dnn_linear.weight, l2=l2_reg_linear)
         self.to(device)
 
 
